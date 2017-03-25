6b1beedb4ef98014a527c8d70a5020097ebddabf
Cody Wall <cody@cmwall.io>
2017-03-25 13:23:48 -0500

    Preliminary styles for loading component
    This commit begins the styling for the loading component by separating the text
    into a new component and adding a spinner.

---
+++ src/components/Results/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="ResultsContainer__loading">
+      <h1>Loading...</h1>
+      <div className="spinner" />
+    </div>
+  );
+};
+
+module.exports = Loading;
